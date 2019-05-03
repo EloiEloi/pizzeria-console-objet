28e74c3d61c92b57faaab7380640e181abe668a6
Eloi Turpin <eloi.turpin@gmail.com>
2019-05-03 11:38:59 +0200

    tp 13

--- src/main/java/fr/pizzeria/model/MenuService.java
+++ src/main/java/fr/pizzeria/model/MenuService.java
@@ -5,12 +5,14 @@ package fr.pizzeria.model;
 
 import java.util.Scanner;
 
+import fr.pizzeria.exception.StockageException;
+
 /**
  * @author Eloi
  *
  */
 public abstract class MenuService {
 
-	public abstract void executeUC(Scanner scanner, IPizzaDao dao);
+	public abstract void executeUC(Scanner scanner, IPizzaDao dao) throws StockageException;
 
 }
--- src/main/java/fr/pizzeria/model/ModifierPizzaService.java
+++ src/main/java/fr/pizzeria/model/ModifierPizzaService.java
@@ -5,6 +5,10 @@ package fr.pizzeria.model;
 
 import java.util.Scanner;
 
+import org.apache.commons.lang3.math.NumberUtils;
+
+import fr.pizzeria.exception.UpdatePizzaException;
+
 /**
  * @author Eloi
  *
@@ -14,31 +18,58 @@ import java.util.Scanner;
 public class ModifierPizzaService extends MenuService {
 
 	@Override
-	public void executeUC(Scanner scanner, IPizzaDao dao) {
+	public void executeUC(Scanner scanner, IPizzaDao dao) throws UpdatePizzaException {
 
 		String valeur = "";
 
 		System.out.println("----- Liste des pizzas -----");
 
 		System.out.println("Veuillez choisir le code de la pizza à modifier");
-		valeur = scanner.nextLine();
+		valeur = scanner.nextLine();// assigne la saisie utilisateur à la variable "valeur"
 		String monCodePizzaAModifier = valeur;
 
 		System.out.println("Veuillez saisir le nouveau code :");
-		valeur = scanner.nextLine();
+		valeur = scanner.nextLine();// assigne la saisie utilisateur à la variable "valeur"
 		String monCode = valeur;
 
 		System.out.println("Veuillez saisir le nouveau nom (sans espace) :");
-		valeur = scanner.nextLine();
+		valeur = scanner.nextLine();// assigne la saisie utilisateur à la variable "valeur"
 		String monLibelle = valeur;
 
 		System.out.println("Veuillez saisir le nouveau prix :");
-		valeur = scanner.nextLine();
-		Double monPrix = Double.parseDouble(valeur);
+		valeur = scanner.nextLine(); // assigne la saisie utilisateur à la variable "valeur"
+		Double monPrix = null;
+		// on test si la valeur entrée est bien un nombre
+		if (NumberUtils.isCreatable(valeur)) {
+			// si oui, on transforme le string en double
+			monPrix = Double.parseDouble(valeur);
+		} else {
+			monPrix = null;
+		}
 
-		Pizza pizzaTemp = new Pizza(monCode, monLibelle, monPrix);
+		String monPrixEnString = valeur;
 
-		dao.updatePizza(monCodePizzaAModifier, pizzaTemp);
+		if (monCodePizzaAModifier.equals("")) {
+			throw new UpdatePizzaException("le code pizza n'est pas correct");
+
+		} else if (monCode.equals("")) {
+			throw new UpdatePizzaException("Le nouveau code n'est pas correct");
+
+		} else if (monLibelle.equals("") || monLibelle.equals("0")) {
+			throw new UpdatePizzaException("Le nouveau libellé n'est pas correct");
+
+		} else if (monPrix.equals("0")) {
+			throw new UpdatePizzaException("Le nouveau prix ne peut pas valoir : 0 ");
+
+		} else if (monPrix == null) {
+			throw new UpdatePizzaException("Le nouveau prix ne peut pas être vide ");
+
+		} else {
+			Pizza pizzaTemp = new Pizza(monCode, monLibelle, monPrix);
+			dao.updatePizza(monCodePizzaAModifier, pizzaTemp);
+
+		}
+
 	}
 
 }
